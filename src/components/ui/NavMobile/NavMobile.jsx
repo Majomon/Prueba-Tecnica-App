65f93940300c2fd1c28b4fb019ec4201676c5e44
Mauri <mauri.monzon91@gmail.com>
2024-07-04 12:33:52 -0300

    Animacion del menu hamburguesa

--- src/components/ui/NavMobile/NavMobile.jsx
+++ src/components/ui/NavMobile/NavMobile.jsx
@@ -2,8 +2,10 @@
 import user from "@/data/user";
 import Image from "next/image";
 import style from "./NavMobile.module.css";
-import { IoMenuOutline } from "react-icons/io5";
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 import { useState } from "react";
+import navLinks from "@/data/navLinks";
+import Link from "next/link";
 
 export const NavMobile = () => {
   const [showModal, setShowModal] = useState(false);
@@ -17,25 +19,27 @@ export const NavMobile = () => {
       <Image
         src="/profile.webp"
         alt="Imagen de perfil"
-        width={1000}
-        height={700}
+        width={40}
+        height={40}
         className={style.img}
       />
       <h1 className={style.h1}>
         {user.firstName} {user.lastName}
       </h1>
       <button className={style.button} onClick={handleClick}>
-        <IoMenuOutline size={30} />
+        {showModal ? <IoCloseOutline size={30} /> : <IoMenuOutline size={30} />}
       </button>
 
-      {showModal && (
-        <div className={style.modalBackdrop} onClick={handleClick}>
-          <div className={style.modalContent}>
-            <h2>Modal</h2>
-            <p>Contenido del modal aquí...</p>
-          </div>
-        </div>
-      )}
+      <ul
+        className={`${style.modalBackdrop} ${showModal ? style.showModal : ""}`}
+        onClick={handleClick}
+      >
+        {navLinks.map((link, index) => (
+          <li key={index} className={style.modalContent}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
